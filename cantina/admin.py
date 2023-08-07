2b619cbfb61dea30b046de6064ea25736f901642
Felipe A <oie.eu.sou.um@gmail.com>
2023-08-06 21:12:51 -0300

    Add `pages` admin management

--- cantina/admin.py
+++ cantina/admin.py
@@ -58,3 +58,59 @@ def category_pages():
             'category_pages': CategoryPage.query.all()
         }
     return render_template('admin/category-pages.html', **context)
+
+
+@app.route("/adeministração/paginas", methods=('GET', 'POST'))
+def pages():
+    page_id = request.args.get('page_id')
+    is_editor = False
+    if page_id is not None:
+        page = Page.query.filter_by(id=page_id).first()
+        if page is None:
+            flash("Página de ID {} não encontrada!".format(page_id), category="error")
+        else:
+            is_editor = True
+        if request.method == "POST":
+            title = request.form.get("title")
+            description = request.form.get("description")
+            route_id = request.form.get("route_id")
+            category_page_id = request.form.get("category_page_id")
+            appear_navbar = request.form.get("appear_navbar") == 'on'
+            if title is not None and title != page.title:
+                flash(f"Nome da página alterada com sucesso ({page.title} -> {title})!", category="success")
+                page.title = title
+            if description is not None and description != page.description:
+                flash(f"Descrição da página alterada com sucesso ({page.description} -> {description})!", category="success")
+                page.description = description
+            if route_id is not None and route_id != str(page.route_id):
+                route = Route.query.filter_by(id=route_id).first()
+                if route is None:
+                    flash("Rota de ID {} não encontrada!".format(route_id), category="error")
+                else:
+                    flash(f"Rota da página alterada com sucesso ({page.route_id} -> {route_id})!", category="success")
+                    page.route_id = route_id
+                    page.route = route
+            if category_page_id is not None and category_page_id != str(page.category_page_id):
+                category_page = CategoryPage.query.filter_by(id=category_page_id).first()
+                if category_page is None:
+                    flash("Categoria de página de ID {} não encontrada!".format(category_page_id), category="error")
+                else:
+                    flash(f"Categoria de página da página alterada com sucesso ({page.category_page_id} -> {category_page_id})!", category="success")
+                    page.category_page_id = category_page_id
+                    page.category_page = category_page
+            
+            if appear_navbar != page.appear_navbar:
+                flash(f"Aparece na Barra de Navegação da página alterada com sucesso ({page.appear_navbar} -> {appear_navbar})!", category="success")
+                page.appear_navbar = appear_navbar
+            db.session.commit()
+        context = {
+            'page': page,
+            'is_editor': is_editor,
+            'routes': Route.query.all(),
+            'category_pages': CategoryPage.query.all()
+        }
+    else:
+        context = {
+            "pages": Page.query.all()
+        }
+    return render_template('admin/pages.html', **context)
