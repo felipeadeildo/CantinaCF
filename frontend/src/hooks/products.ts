2e21f984aa811b9ad640b1b1fd06def187034cd8
Felipe Adeildo <felipe.adeildo@proton.me>
2024-03-24 13:06:06 -0300

    feat: Optimistic Update vs Pessimistic Update

--- frontend/src/hooks/products.ts
+++ frontend/src/hooks/products.ts
@@ -35,14 +35,112 @@ export const useProductsMutation = () => {
 
   const addProductToCartMutation = useMutation({
     mutationFn: (productId: number) => addProductToCart(token, productId),
+    onMutate: async (productId: number) => {
+      await queryClient.cancelQueries({ queryKey: ["cart"] })
+      await queryClient.cancelQueries({ queryKey: ["products", undefined] })
+
+      const previousCart = queryClient.getQueryData<TCart[]>(["cart"])
+      queryClient.setQueryData<TCart[]>(["cart"], (oldCart) => {
+        const item = (oldCart || []).find((item) => item.product.id === productId)
+        if (!item) return oldCart
+
+        return oldCart?.map((cartItem) => {
+          if (cartItem.product.id === productId) {
+            return {
+              ...cartItem,
+              quantity: cartItem.quantity + 1,
+            }
+          }
+          return cartItem
+        })
+      })
+
+      const previousProductsQueries = queryClient.getQueriesData<TProduct[]>({
+        queryKey: ["products"],
+      })
+
+      previousProductsQueries.forEach(([queryKey, oldProducts]) => {
+        queryClient.setQueryData<{ products?: TProduct[]; message?: string }>(
+          queryKey,
+          (currentData) => {
+            return {
+              ...currentData,
+              products: currentData?.products?.map((product) =>
+                product.id === productId
+                  ? { ...product, quantity: product.quantity - 1 }
+                  : product
+              ),
+            }
+          }
+        )
+      })
+
+      // para possível rollback
+      return { previousCart, previousProductsQueries }
+    },
+    onError: (_, __, context) => {
+      queryClient.setQueryData(["cart"], context?.previousCart)
+      context?.previousProductsQueries.forEach((queryKey, oldProducts) => {
+        queryClient.setQueryData(queryKey, oldProducts)
+      })
+    },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["products"] })
+      queryClient.invalidateQueries({ queryKey: ["products", undefined], exact: false })
       queryClient.invalidateQueries({ queryKey: ["cart"] })
     },
   })
 
   const removeProductFromCartMutation = useMutation({
     mutationFn: (productId: number) => removeProductFromCart(token, productId),
+    onMutate: async (productId: number) => {
+      await queryClient.cancelQueries({ queryKey: ["cart"] })
+      await queryClient.cancelQueries({ queryKey: ["products", undefined] })
+
+      const previousCart = queryClient.getQueryData<TCart[]>(["cart"])
+      queryClient.setQueryData<TCart[]>(["cart"], (oldCart) => {
+        const item = (oldCart || []).find((item) => item.product.id === productId)
+        if (!item) return oldCart
+
+        return oldCart?.map((cartItem) => {
+          if (cartItem.product.id === productId) {
+            return {
+              ...cartItem,
+              quantity: cartItem.quantity - 1,
+            }
+          }
+          return cartItem
+        })
+      })
+
+      const previousProductsQueries = queryClient.getQueriesData<TProduct[]>({
+        queryKey: ["products"],
+      })
+
+      previousProductsQueries.forEach(([queryKey, oldProducts]) => {
+        queryClient.setQueryData<{ products?: TProduct[]; message?: string }>(
+          queryKey,
+          (currentData) => {
+            return {
+              ...currentData,
+              products: currentData?.products?.map((product) =>
+                product.id === productId
+                  ? { ...product, quantity: product.quantity + 1 }
+                  : product
+              ),
+            }
+          }
+        )
+      })
+
+      // para possível rollback
+      return { previousCart, previousProductsQueries }
+    },
+    onError: (_, __, context) => {
+      queryClient.setQueryData(["cart"], context?.previousCart)
+      context?.previousProductsQueries.forEach((queryKey, oldProducts) => {
+        queryClient.setQueryData(queryKey, oldProducts)
+      })
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["products"] })
       queryClient.invalidateQueries({ queryKey: ["cart"] })
