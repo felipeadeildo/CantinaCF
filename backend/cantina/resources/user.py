bfa5808e3b85ba79ad3481afd74062a2b3cebb59
Felipe Adeildo <felipe.adeildo@proton.me>
2024-04-30 17:35:35 -0300

    refactor+feat: Move `POST /api/users` to `POST /api/user` and implements `PUT /api/user` (update user)

--- backend/cantina/resources/user.py
+++ backend/cantina/resources/user.py
@@ -20,6 +20,50 @@ class UserResource(Resource):
 
         return {"user": user.as_dict()}
 
+    @jwt_required()
+    def post(self):
+        data = request.json
+        if not data:
+            return {"message": "Nenhum dado enviado."}, 400
+
+        user = User.query.filter_by(username=data.get("username")).first()
+        if user:
+            return {"message": f"Usuário {user.username} já existe."}, 400
+
+        if data.get("role_id") == "3":
+            user = User.query.filter_by(matricula=data.get("matricula")).first()
+            if user:
+                return {"message": f"Matrícula {user.matricula} já existe."}, 400
+
+        try:
+            # TODO: make it more secure
+            user = User(**data)
+            db.session.add(user)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+            return {"message": "Erro ao criar o usuário."}, 500
+
+        return {
+            "message": f"Usuário {user.name} criado com sucesso.",
+            "user": user.as_dict(),
+        }, 201
+
+    @jwt_required()
+    def put(self):
+        data = request.json
+        if not data:
+            return {"message": "Nenhum dado enviado."}, 400
+        user = User.query.filter_by(id=data.get("id")).first()
+        if user is None:
+            return {"message": "Usuário não encontrado."}, 404
+
+        for key, value in data.items():
+            # TODO: make it more secure (password can be set directly?!)
+            setattr(user, key, value)
+        db.session.commit()
+        return {"message": "Usuário atualizado com sucesso.", "user": user.as_dict()}
+
 
 class UsersResource(Resource):
     @jwt_required()
@@ -48,31 +92,3 @@ class UsersResource(Resource):
         users = user_query.all()
 
         return {"users": [user.as_dict() for user in users]}
-
-    @jwt_required()
-    def post(self):
-        data = request.json
-        if not data:
-            return {"message": "Nenhum dado enviado."}, 400
-
-        user = User.query.filter_by(username=data.get("username")).first()
-        if user:
-            return {"message": f"Usuário {user.username} já existe."}, 400
-
-        if data.get("role_id") == "3":
-            user = User.query.filter_by(matricula=data.get("matricula")).first()
-            if user:
-                return {"message": f"Matrícula {user.matricula} já existe."}, 400
-
-        try:
-            user = User(**data)
-            db.session.add(user)
-            db.session.commit()
-        except Exception as e:
-            print(e)
-            return {"message": "Erro ao criar o usuário."}, 500
-
-        return {
-            "message": f"Usuário {user.name} criado com sucesso.",
-            "user": user.as_dict(),
-        }, 201
