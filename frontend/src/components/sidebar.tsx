6885d8b6377aa7800b9c71c5163849c0a2574299
Felipe Adeildo <oie.eu.sou.um@gmail.com>
2024-05-18 21:29:01 -0300

    refactor[front]: Sidebar

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -8,20 +8,27 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet"
 
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
-
 import { SIDEBAR_PAGES, SIDEBAR_PAGES_CATEGORIES } from "@/constants/pages"
-import { AlignLeftIcon, LogOut, LogOutIcon } from "lucide-react"
+import { useAuth } from "@/contexts/auth"
+import { AlignLeftIcon, LogOutIcon } from "lucide-react"
 import Link from "next/link"
-import { useState } from "react"
+import { useCallback, useState } from "react"
 
 export const SideBar = () => {
   const [open, setOpen] = useState(false)
+  const { user } = useAuth()
+
+  const getPages = useCallback(
+    (category: string) => {
+      return SIDEBAR_PAGES.filter(
+        (page) =>
+          page.allowed_roles.includes(user?.role_id as number) &&
+          page.category === category
+      )
+    },
+    [user]
+  )
+
   return (
     <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger asChild>
@@ -31,49 +38,50 @@ export const SideBar = () => {
       </SheetTrigger>
       <SheetContent>
         <SheetHeader className="border-b pb-4">
-          <SheetTitle className="text-xl font-semibold text-gray-900 dark:text-white">
-            CantinaCF
-          </SheetTitle>
-          <SheetDescription className="text-sm text-gray-500 dark:text-gray-400">
+          <SheetTitle className="text-xl font-semibold text-center">CantinaCF</SheetTitle>
+          <SheetDescription className="text-sm text-center">
             Feito com ❤️ por Felipe Adeildo
           </SheetDescription>
         </SheetHeader>
-        <div className="pt-4">
+        <div className="overflow-y-auto max-h-[calc(100vh-120px)]">
           {SIDEBAR_PAGES_CATEGORIES.map((category) => (
-            <Accordion type="single" collapsible key={category}>
-              <AccordionItem value={category}>
-                <AccordionTrigger>
-                  <span className="font-medium">{category}</span>
-                </AccordionTrigger>
-                <AccordionContent className="flex flex-col gap-1">
-                  {SIDEBAR_PAGES.filter((page) => page.category === category).map(
-                    (page) => (
-                      <Link
-                        key={page.path}
-                        href={page.path}
-                        className="px-4 py-2 text-sm  rounded-md"
-                        onClick={() => setOpen(false)}
-                      >
-                        {page.name}
-                      </Link>
-                    )
-                  )}
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
+            <div key={category}>
+              {getPages(category).length > 0 && (
+                <p className="font-semibold text-sm text-center py-2">{category}</p>
+              )}
+              <div className="px-3 space-y-2">
+                {getPages(category).map((page) => (
+                  <Button
+                    key={page.path}
+                    variant="ghost"
+                    className="w-full justify-start border-t-2"
+                  >
+                    <Link
+                      href={page.path}
+                      className="flex items-center gap-3 text-sm font-medium"
+                      onClick={() => setOpen(false)}
+                    >
+                      <page.icon size={18} />
+                      {page.name}
+                    </Link>
+                  </Button>
+                ))}
+              </div>
+            </div>
           ))}
+          <div className="mt-6 border-t pt-4">
+            <Button variant="ghost" asChild>
+              <Link
+                href="/auth/logout"
+                className="w-full flex gap-1 justify-center items-center"
+                onClick={() => setOpen(false)}
+              >
+                <LogOutIcon size={18} />
+                Sair
+              </Link>
+            </Button>
+          </div>
         </div>
-
-        <Button variant="ghost" asChild>
-          <Link
-            href="/auth/logout"
-            className="w-full flex gap-1 justify-center items-center text-destructive hover:text-destructive/80"
-            onClick={() => setOpen(false)}
-          >
-            <LogOutIcon size={18} />
-            Sair
-          </Link>
-        </Button>
       </SheetContent>
     </Sheet>
   )
