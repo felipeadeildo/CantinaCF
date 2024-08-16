e6f3b19914ee6f57886b8b097e32e52d9442a7cc
Felipe Adeildo <felipeadeildo@duck.com>
2024-08-16 11:07:27 -0300

    fix(back): não inclui pagamentos do tipo pix

--- backend/cantina/resources/payments.py
+++ backend/cantina/resources/payments.py
@@ -1,11 +1,14 @@
 from cantina.models import Payment
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
+from sqlalchemy import and_
 
 
 class PaymentsResource(Resource):
     @jwt_required()
     def get(self):
-        payments = Payment.query.filter_by(status="to allow").all()
+        payments = Payment.query.filter(
+            and_(Payment.status == "to allow", Payment.payment_method_id != 1)
+        ).all()
 
         return {"payments": [p.as_dict() for p in payments]}
