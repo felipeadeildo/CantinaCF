cffb78063191babf0fa65aacf2a0daa4d23db61e
Felipe Adeildo <felipeadeildo@duck.com>
2024-09-08 15:46:04 -0300

    fix(back): adjust expiration date format and add timezone information.

--- backend/cantina/services/bank.py
+++ backend/cantina/services/bank.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from uuid import uuid4
 
 import mercadopago
@@ -11,7 +11,17 @@ sdk = mercadopago.SDK(MERCADO_PAGO_ACCESS_TOKEN)
 
 def generate_pix_payment(payment: Payment, user: User) -> dict:
     options = RequestOptions(custom_headers={"x-idempotency-key": str(uuid4())})
-    expiration_date = (datetime.now() + timedelta(minutes=10)).isoformat() + "-03:00"
+
+    expiration_date = datetime.now(timezone(timedelta(hours=-3))) + timedelta(
+        minutes=10
+    )
+
+    expiration_date_str = expiration_date.strftime("%Y-%m-%dT%H:%M:%S.%f")[
+        :-3
+    ] + expiration_date.strftime("%z")
+
+    # Ajustar o formato do offset para ter o ':' (de "-0300" para "-03:00")
+    expiration_date_str = expiration_date_str[:-2] + ":" + expiration_date_str[-2:]
 
     payment_data = {
         "transaction_amount": float(payment.value),
@@ -27,14 +37,13 @@ def generate_pix_payment(payment: Payment, user: User) -> dict:
             "user_id": user.id,
         },
         "external_reference": payment.id,
-        "date_of_expiration": expiration_date,
+        "date_of_expiration": expiration_date_str,
     }
 
     result = sdk.payment().create(payment_data, options)
-    print(result)
-    result["response"]["point_of_interaction"]["transation_data"]["expiration_date"] = (
-        expiration_date
-    )
+    result["response"]["point_of_interaction"]["transaction_data"][
+        "expiration_date"
+    ] = expiration_date_str
 
     return result["response"]["point_of_interaction"]
 
